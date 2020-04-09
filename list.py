163353e5bebe6a7724b8e0b22229096e651c8c64
Nelson Vergara <nelsonv_chile@hotmail.com>
2020-04-09 12:02:31 -0400

    add6

--- list.py
+++ list.py
@@ -1,4 +1,6 @@
 import boto.ec2
+import boto.s3
+
 conn=boto.ec2.connect_to_region("us-east-1")
 reservations = conn.get_all_instances()
 for res in reservations:
@@ -7,6 +9,15 @@ for res in reservations:
             print "%s (%s) [%s]" % (inst.tags['Name'], inst.id, inst.state)
         else:
             print "%s [%s]" % (inst.id, inst.state)
+			
+conn=boto.s3.connect_to_region("us-east-1")
+reservations2 = conn.get_all_instances()
+for res in reservations2:
+    for inst in res.instances:
+        if 'Name' in inst.tags:
+            print "%s (%s) [%s]" % (inst.tags['Name'], inst.id, inst.state)
+        else:
+            print "%s [%s]" % (inst.id, inst.state)			
 			
 			
 			
