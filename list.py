a8ba64429eeb7d267ccb1bf0cbd90bf1fc7768f6
Nelson Vergara <nelsonv_chile@hotmail.com>
2020-04-09 11:27:52 -0400

    add3

---
+++ list.py
@@ -0,0 +1,10 @@
+import boto.ec2
+conn=boto.ec2.connect_to_region("us-east-1")
+reservations = conn.get_all_instances()
+for res in reservations:
+    for inst in res.instances:
+        if 'Name' in inst.tags:
+            print "%s (%s) [%s]" % (inst.tags['Name'], inst.id, inst.state)
+        else:
+            print "%s [%s]" % (inst.id, inst.state)
+			
